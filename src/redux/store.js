69cb3d9fa808b3e909444fffefb73bb77f44690b
Luke Elabor <Lukemane@boxfusion.co.za>
2019-12-25 00:29:15 +0200

    store change

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from './reducer';
-import thunk from 'redux-thunk';
 
 const initialState = {
   users: {},
   company: {}
 };
-export const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+export const store = createStore(rootReducer, initialState);
